c84dd998fdbd4131a3caa73f4e5c5bfa750c4a9c
Nirmal <nirmal99.ba@gmail.com>
2023-01-04 16:34:40 +0545

    Added first successful publish

--- example/mapbox_draw_example/lib/main.dart
+++ example/mapbox_draw_example/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:mapbox_draw_polygon_area/src/mapbox_draw_polygon.dart'
     as myPacakge;
-import 'package:mapbox_gl/mapbox_gl.dart';
+import 'package:mapbox_gl_modified/mapbox_gl_modified.dart';
 
 void main() => runApp(MyApp());
 
